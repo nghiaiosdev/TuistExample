777a86e370697e116715dee285765139b6fccc63
Nguyen Nghia <nghiaklpro195@gmail.com>
2020-11-05 16:29:20 +0700

    Initial Commit

---
+++ MySwiftAp/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  MySwiftAp
+//
+//  Created by Nghia Nguyen on 11/5/20.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ MySwiftAp/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  MySwiftAp
+//
+//  Created by Nghia Nguyen on 11/5/20.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
